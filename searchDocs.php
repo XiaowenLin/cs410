8653a7c51bc952bf980d2166cc6f8dfbf91b38b0
asnyder6 <asnyder6@illinois.edu>
2016-04-25 08:48:00 -0500

    Update searchDocs.php

--- searchDocs.php
+++ searchDocs.php
@@ -9,22 +9,4 @@ $nearVal  = rawurldecode($_GET['nearVal']);
 //echo the inputs for a test
 echo "Find: {$findVal} Near: {$nearVal}";
 return;
-//awaiting java function details
-/*
-//try {
-   //Initialize the java connection
-//   $javaConn = new Java(''); //Initialize the java connection
-//   if($javaConn === NULL) {
-//      echo "ERROR creating java connection";
-//      return;
-//   }
-
-   // Run the query
-//   $result = $javaConn->searchDocs($findVal, $nearVal);
-
-//} catch (Exception $e) {
-//   echo "ERROR " . $e;
-//   return;
-//} 
-*/
 ?>
