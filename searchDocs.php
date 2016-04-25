e2849ee6ef45cb25e6b8efca8fa1d07fad38bc05
asnyder6 <asnyder6@illinois.edu>
2016-04-25 08:40:18 -0500

    Create searchDocs.php

---
+++ searchDocs.php
@@ -0,0 +1,28 @@
+<?php
+/************************************
+Author: Aaron Snyder
+Date: 4/25/16
+************************************/
+//retrieve user inputs
+$findVal  = rawurldecode($_GET['findVal']);
+$nearVal  = rawurldecode($_GET['nearVal']);
+//echo the inputs for a test
+echo "Find: {$findVal} Near: {$nearVal}";
+/*awaiting java function details
+try {
+   //Initialize the java connection
+   $javaConn = new Java(''); //Initialize the java connection
+   if($javaConn === NULL) {
+      echo "ERROR creating java connection";
+      return;
+   }
+
+   // Run the query
+   $result = $javaConn->searchDocs($findVal, $nearVal);
+
+} catch (Exception $e) {
+   echo "ERROR " . $e;
+   return;
+} 
+*/
+?>
